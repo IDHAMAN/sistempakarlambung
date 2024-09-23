5d395e88dada146dfb0a3fb7adeb0210b8a73b25
Zahravi Idham Muhardian <116077525+IDHAMAN@users.noreply.github.com>
2024-09-24 01:20:40 +0700

    Update register.php
    Perubahan pada menu nama

--- register.php
+++ register.php
@@ -9,7 +9,6 @@ if (isset($_SESSION['role'])) {
         header("location: indexPakar.php");
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,12 +23,12 @@ if (isset($_SESSION['role'])) {
 <link rel="stylesheet" href="custom.css" />
 
 <!--Font-->
-<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap"rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap" rel="stylesheet" />
 <script src="https://kit.fontawesome.com/yourcode.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-<title>Cek lambung !</title>
+<title>Cek Lambung !</title>
 </head>
 
 <body>
@@ -53,12 +52,12 @@ if (isset($_SESSION['role'])) {
                     </div>
                     <div class="col">
                         <label class="papan" for="nama">Nama Pasien</label>
-                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama" required>
+                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama" required pattern="[A-Za-z\s]+" title="Nama tidak boleh mengandung angka">
                         <div class="valid-feedback">
                             Bagus!
                         </div>
                         <div class="invalid-feedback">
-                            Nama tidak boleh kosong
+                            Nama tidak boleh kosong dan tidak boleh mengandung angka
                         </div>
                     </div>
                 </div>
@@ -118,33 +117,30 @@ if (isset($_SESSION['role'])) {
     </div>
 </div>
 
-
-
-
 <!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Set maximum date for Tanggal Lahir to today's date
+    const today = new Date().toISOString().split('T')[0];
+    document.getElementById('tgl_lahir').setAttribute('max', today);
 
-<script type="text/javascript">
-(function() {
-'use strict';
-window.addEventListener('load', function() {
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-var forms = document.getElementsByClassName('needs-validation');
-// Loop over them and prevent submission
-var validation = Array.prototype.filter.call(forms, function(form) {
-    form.addEventListener('submit', function(event) {
-    if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
-    form.classList.add('was-validated');
-    }, false);
+    // Bootstrap validation
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            var forms = document.getElementsByClassName('needs-validation');
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
 });
-}, false);
-})();
 </script>
 </body>
 
